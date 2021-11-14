84584c9050fc97eb2faf6c09fe7f926a59a8badb
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-11-14 12:14:58 -0500

    commit tree 6744f9a8faa83856486af47955d1c760cfe24c0f

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+            return { friends: [...state.friends, action.friend] };
+        case "REMOVE_FRIEND":
+            const newArray = state.friends.filter((friend) => {return friend.id !== action.id });
+            return {friends: newArray}
+        default:
+            return state;
+    }
 }
